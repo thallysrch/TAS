193d63736a779290e9ca673b5b1e798b28f2fa89
PICHAU <PICHAU@Thallys-PC>
2020-09-16 20:55:33 -0300

    Aula 09/09/2020 - 2

--- tas-server/src/main/java/br/edu/materdei/tas/controller/GrupoController.java
+++ tas-server/src/main/java/br/edu/materdei/tas/controller/GrupoController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,6 +22,16 @@ public class GrupoController {
         List<GrupoEntity> grupos = service.findAll();
         
         return new ResponseEntity<List<GrupoEntity>>(grupos, HttpStatus.OK);        
+    }
+    
+    @PostMapping("grupos")
+    public ResponseEntity create(@RequestBody GrupoEntity grupo){
+        try{
+            this.service.save(grupo);
+            return new ResponseEntity(grupo, HttpStatus.CREATED);
+        }   catch (Exception e){
+            return new ResponseEntity("", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     }
     
 }
