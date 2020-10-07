c3d41b09889a0676ae008169733bd6916b5b88ed
PICHAU <PICHAU@Thallys-PC>
2020-10-06 21:41:17 -0300

    Aula 30/09

--- tas-venda/src/main/java/br/edu/materdei/tas/venda/entity/ItemPedidoEntity.java
+++ tas-venda/src/main/java/br/edu/materdei/tas/venda/entity/ItemPedidoEntity.java
@@ -4,6 +4,7 @@ import br.edu.materdei.tas.core.entity.ProdutoEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -12,7 +13,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "itempedido")
 public class ItemPedidoEntity {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Integer id;
     
     @Column(nullable = false)
